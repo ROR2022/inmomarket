2fc2d2b2c6fabb017af96da37ce49adc4ec6e22a
Ramiro Ocampo Rodriguez <101475348+ROR2022@users.noreply.github.com>
2025-04-14 16:49:21 -0600

    explorar improved

--- app/(auth-pages)/logout/page.tsx
+++ app/(auth-pages)/logout/page.tsx
@@ -1,11 +1,16 @@
 'use client';
 
+import { useState } from 'react';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import { signOutAction } from '@/app/actions';
+import { Loader2 } from 'lucide-react';
 export default function Logout() {
+  const [isLoading, setIsLoading] = useState(false);
   const handleLogout = async () => {
+    setIsLoading(true);
     await signOutAction();
+    setIsLoading(false);
   };
   return (
     <div className="flex flex-col items-center justify-center w-full mt-10">
@@ -14,7 +19,10 @@ export default function Logout() {
         <Link href="/" className="text-sm text-muted-foreground">
           Volver al inicio
         </Link>
-        <Button onClick={handleLogout}>Cerrar sesión</Button>
+        <Button onClick={handleLogout} disabled={isLoading}>
+          {isLoading ? 'Cerrando sesión...' : 'Cerrar sesión'}
+          {isLoading && <Loader2 className="w-4 h-4 ml-2" />}
+        </Button>
       </div>
     </div>
   );
